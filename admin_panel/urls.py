9ddde55d6d7a29bfbb5b1cb85f9a7950f13173c1
Abduaziz3455 <abduaziz3455@gmail.com>
2023-11-17 18:00:12 +0500

    client visits

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 
 from admin_panel.swagger_views import DecoratedTokenRefreshView, DecoratedTokenObtainPairView
 from admin_panel.views import UserViewSet, ClientEmployeeViewSet, ClientEmployeeTimeViewSet, CameraViewSet, \
-    RunCameraView
+    RunCameraView, ClientVisitStat
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -17,5 +17,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('login/', DecoratedTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('cam_start_stop/', RunCameraView.as_view(), name='camera_start_stop'),
+    path('client_visits/', ClientVisitStat.as_view(), name='client_visits'),
     path('login/refresh/', DecoratedTokenRefreshView.as_view(), name='token_refresh'),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,10 +1,14 @@
 import threading
+from datetime import datetime
 
+from django.db.models import Count, Q
+from django.db.models.functions import TruncDay
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_yasg import renderers
 from rest_framework import filters as searchf
 from rest_framework import permissions, parsers
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -13,7 +17,7 @@ from admin_panel.custom_permissions import IsAdminUser, IsSuperUser, IsExpired
 from admin_panel.custom_viewsets import Custom_Viewsub, Client_Viewsub, Camera_Viewsub
 from admin_panel.models import CustomUser, ClientEmployee, ClientEmployeeTime, Camera
 from admin_panel.serializers import UserSerializer, ClientEmployeeSerializer, ClientEmployeeTimeSerializer, \
-    CameraSerializer
+    CameraSerializer, ClientStat_Serial
 from smart_cam.main_video import Face_App
 
 camera_list = [{'ip_address': '192.168.1.64', 'login': 'admin', 'password': 'softex2020', 'is_enter': True, 'real': 1}]
@@ -95,3 +99,64 @@ class RunCameraView(APIView):
 
         else:
             return Response({'error': 'Invalid action'})
+
+
+class ClientVisitStat(GenericAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = ClientEmployee.objects.order_by('id')
+    serializer_class = ClientEmployeeSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['is_client', 'last_time']
+
+    def get(self, request):
+        daily_stats = self.get_queryset().annotate(time=TruncDay('created_time')) \
+            .annotate(first_visit=Count('id', filter=Q(enter_count=1))) \
+            .annotate(re_visit=Count('id', filter=Q(enter_count__gt=1))). \
+            filter(status=True, is_client=True)
+
+        # if start_date:
+        #     start_date = datetime.strptime(start_date, "%m.%Y").date()
+        #     daily_stats = daily_stats.filter(
+        #         Q(created_date__month=start_date.month) & Q(created_date__year=start_date.year))
+
+        context = {'date': 'day'}
+        serializer = ClientStat_Serial(daily_stats.order_by('-created_time'), many=True, context=context)
+        dicts = serializer.data
+        #
+        cum_list = []
+        for dictionary in dicts:
+            time = dictionary['time']
+            order_completed = dictionary['first_visit']
+            order_rejected = dictionary['re_visit']
+            found = False
+            for one_dict in cum_list:
+                if one_dict['time'] == time:
+                    one_dict['first_visit'] += order_completed
+                    one_dict['re_visit'] += order_rejected
+                    found = True
+                    break
+
+            if not found:
+                cumulative_sums = {
+                    'time': time,
+                    'first_visit': order_completed,
+                    're_visit': order_rejected
+                }
+                cum_list.append(cumulative_sums)
+        times = []
+        for i in cum_list:
+            time = i['time']
+            times.append(time)
+        for i in range(1, 32):
+            if i in times:
+                continue
+            else:
+                cumulative_sums = {
+                    'time': i,
+                    'first_visit': 0,
+                    're_visit': 0
+                }
+                cum_list.append(cumulative_sums)
+        cum_list = sorted(cum_list, key=lambda x: x["time"])
+
+        return Response(cum_list)
